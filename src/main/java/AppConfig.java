b2dcb9e031473585f466f25ba7c4793fcc9094ec
Gala Arbatskaya <galinaarbatskaya0@gmail.com>
2023-05-02 16:49:34 +0300

    Commit1

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,12 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat Cat = new Cat();
+        Cat.setMessage("It is a Cat");
+        return Cat;
+    }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,26 @@
+import java.util.Objects;
+
+public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cat cat = (Cat) o;
+        return Objects.equals(message, cat.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return message != null ? message.hashCode() : 0;
+    }
+
+}
